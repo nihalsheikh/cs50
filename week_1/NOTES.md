00d7132f0af8fcd2711ddba21ac7ee36acb94ab8
nihalsheikh <nihalsheikh585@gmail.com>
2024-06-20 16:46:13 +0530

    Loop code and Notes updated

--- week_1/NOTES.md
+++ week_1/NOTES.md
@@ -91,3 +91,10 @@ Example:
         }
     }
 ```
+
+### Important:
+- C lang taken everything literally, The C compiler reads from Top to Down, and will execute the Main Function first.
+
+- It is imp to remember this because if you define another function below main, it may throw an error, as it may not find it in the file.
+
+- To avoid this situation, we make that another function above the Main function, so it knows, that another function exists in the code.
--- week_1/codes/loops/woof.c
+++ week_1/codes/loops/woof.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-/*
-C lang taken everything literally, The C compiler reads from
-Top to Down, and will execute the Main Function first.
-
-It is imp to remember this because if you define another
-function below main, it may throw an error, as it may not find
-it in the file.
-
-To avoid this situation, we make that another function above
-the Main function, so it knows, that another function
-exists in the code.
-*/
-
 void woof(int n);
 
 int main(void) {
