712d6b44174d4ccda83959ec1f1fc9aca6689db8
nihalsheikh <nihalsheikh585@gmail.com>
2024-06-20 15:54:00 +0530

    Loops code added

--- week_1/codes/loops/meow.c
+++ week_1/codes/loops/meow.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
+    // remember the value of i, it should be less when declaring int i than in the while loop
     int i = 1;
     while (i <= 3) {
         printf("meow\n");
