19cd0e027e478a8b2fdcb2abf99509009ebbe0e5
nihalsheikh <nihalsheikh585@gmail.com>
2024-06-20 15:57:44 +0530

    Loops code changed

--- week_1/codes/loops/meow.c
+++ week_1/codes/loops/meow.c
@@ -2,8 +2,11 @@
 
 int main(void) {
     // remember the value of i, it should be less when declaring int i than in the while loop
-    int i = 1;
-    while (i <= 3) {
+    // best practise is to write value from 0, in this case int i should be equal to 0
+    // remeber, that when starting value is 0, then loop is either '>' or '<' than, never include '=' sign
+
+    int i = 0;
+    while (i < 3) {
         printf("meow\n");
         i++;
     }
